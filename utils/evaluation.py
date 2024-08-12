a22de36013d4a8a6beea45e15858a27e2382a6e0
theAayushbajaj <aayushbajaj58@gmail.com>
2024-08-12 18:51:41 -0400

    added monte-carlo samples and uncertainty estimates for mcdropout

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -5,6 +5,7 @@ import sklearn.metrics as sklm
 import numpy as np
 from scipy.interpolate import interp1d
 import warnings
+import sys
 warnings.filterwarnings('ignore')
 
 
@@ -249,7 +250,54 @@ def expected_calibration_error(pred_probs, labels, num_bins=10, metric_variant="
         result = np.sqrt(result)
     return result
 
-# TODO: Implement softmax calibration error
+# TODO: Implement mcdropout
+def enable_dropout(model):
+    """ Function to enable the dropout layers during test-time """
+    for m in model.modules():
+        if m.__class__.__name__.startswith('Dropout'):
+            m.train()
+
+def get_monte_carlo_predictions(data_loader,
+                                forward_passes,
+                                model,
+                                n_classes,
+                                n_samples):
+    """ Function to get the monte-carlo samples and uncertainty estimates
+    through multiple forward passes
+    """
+
+    dropout_predictions = np.empty((0, n_samples, n_classes))
+    softmax = nn.Softmax(dim=1)
+    for i in range(forward_passes):
+        predictions = np.empty((0, n_classes))
+        model.eval()
+        enable_dropout(model)
+        for i, (image, label) in enumerate(data_loader):
+            image = image.to(torch.device('cuda'))
+            with torch.no_grad():
+                output = model(image)
+                output = softmax(output)  # shape (n_samples, n_classes)
+            predictions = np.vstack((predictions, output.cpu().numpy()))
+
+        dropout_predictions = np.vstack((dropout_predictions,
+                                         predictions[np.newaxis, :, :]))
+        # dropout predictions - shape (forward_passes, n_samples, n_classes)
+
+    # Calculating mean across multiple MCD forward passes 
+    mean = np.mean(dropout_predictions, axis=0)  # shape (n_samples, n_classes)
+
+    # Calculating variance across multiple MCD forward passes 
+    variance = np.var(dropout_predictions, axis=0)  # shape (n_samples, n_classes)
+
+    epsilon = sys.float_info.min
+    # Calculating entropy across multiple MCD forward passes 
+    entropy = -np.sum(mean * np.log(mean + epsilon), axis=-1)  # shape (n_samples,)
+
+    # Calculating mutual information across multiple MCD forward passes 
+    mutual_info = entropy - np.mean(np.sum(-dropout_predictions * np.log(dropout_predictions + epsilon),
+                                           axis=-1), axis=0)  # shape (n_samples,)
+
+    return mean, variance, entropy, mutual_info
 
 
 def fnr_fpr_spe_sens_groups(preds, labels, attrs, sens_classes, specificity_val = 0.8, threshold = 0.5):
